94c52bc4aefe7ce44519f28a973ed02bfe35bceb
onkyoh <adnanradwan_8@hotmail.com>
2024-06-14 17:04:12 -0400

    testing email keys

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
+
+async function checkConnection() {
+    try {
+        // Try to make a simple query
+        await prisma.$connect()
+        console.log('Connected to the database successfully')
+    } catch (error) {
+        console.error('Failed to connect to the database', error)
+    } finally {
+        // Optionally, you can disconnect from the database after the check
+        await prisma.$disconnect()
+    }
+}
+
+checkConnection()
 
 export default prisma
